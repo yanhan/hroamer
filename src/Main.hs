0e329c717a86da6b160277cb32f73ee7a049ef2d
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2017-10-03 08:28:02 +0800

    [MISC] Fallback to vim if EDITOR=""
    An additional countermeasure
    
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- src/Main.hs
+++ src/Main.hs
@@ -73,11 +73,16 @@ make_editor_createprocess :: FilePath -> IO CreateProcess
 make_editor_createprocess file = do
   maybe_editor_env_var <- lookupEnv "EDITOR"
   case maybe_editor_env_var of
+    Just "" ->
+      -- Fallback to vim
+      return launch_vim
     Just editor_env_var ->
       return $ shell $ editor_env_var <> " " <> file
     Nothing ->
       -- Fallback to vim
-      return $ proc "vim" [file]
+      return launch_vim
+  where
+    launch_vim = proc "vim" [file]
 
 
 main :: IO ()
