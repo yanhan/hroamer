cb343a72ae64266116beacb3f1f6e14cd1d018af
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2017-10-26 23:40:59 +0800

    [R] Shift `getUnsupportedPaths` to Hroamer.UnsupportedPaths
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- src/Hroamer/UnsupportedPaths.hs
+++ src/Hroamer/UnsupportedPaths.hs
@@ -1,10 +1,14 @@
 module Hroamer.UnsupportedPaths
   ( getDuplicateFilenames
+  , getUnsupportedPaths
   ) where
 
+import Control.Monad (foldM)
 import Data.Set (Set, empty, insert, member)
 import Foundation
-import System.FilePath.Posix (FilePath)
+import System.Directory (canonicalizePath)
+import System.FilePath.Posix
+       ((</>), FilePath, isAbsolute, isValid, takeDirectory)
 
 getDuplicateFilenames :: [FilePath] -> Set FilePath
 getDuplicateFilenames =
@@ -15,3 +19,24 @@ getDuplicateFilenames =
          then (insert fname dup_fnames, all_fnames)
          else (dup_fnames, insert fname all_fnames))
     (empty, empty)
+
+getUnsupportedPaths :: FilePath
+                    -> [FilePath]
+                    -> IO (Set FilePath, Set FilePath, Set FilePath)
+getUnsupportedPaths cwd =
+  foldM
+    (\acc@(abs_paths, files_not_in_cwd, invalid_paths) fname ->
+       if isAbsolute fname
+         then return (insert fname abs_paths, files_not_in_cwd, invalid_paths)
+         else do
+           path <- canonicalizePath $ cwd </> fname
+           if not $ isValid path
+             then return $
+                  (abs_paths, files_not_in_cwd, insert fname invalid_paths)
+             else if takeDirectory path /= cwd
+                    then return $
+                         ( abs_paths
+                         , insert fname files_not_in_cwd
+                         , invalid_paths)
+                    else return acc)
+    (empty, empty, empty)
