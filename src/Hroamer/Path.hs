123cf711d1012e253e98d1608d608ba22cbf4eda
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2017-10-29 20:53:58 +0800

    [R] Shift appendSlashToDir to Hroamer.Path module
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- src/Hroamer/Path.hs
+++ src/Hroamer/Path.hs
@@ -1,9 +1,12 @@
 module Hroamer.Path
-  ( isWeakAncestorDir
+  ( appendSlashToDir
+  , isWeakAncestorDir
   ) where
 
 import Foundation
-import System.FilePath.Posix (FilePath, takeDirectory)
+import System.Directory (doesDirectoryExist)
+import System.FilePath.Posix
+       (FilePath, (</>), addTrailingPathSeparator, takeDirectory)
 
 isWeakAncestorDir :: FilePath -> FilePath -> Bool
 isWeakAncestorDir suspected_ancestor "/" = suspected_ancestor == "/"
@@ -11,3 +14,10 @@ isWeakAncestorDir suspected_ancestor dir_of_interest =
   if suspected_ancestor == dir_of_interest
     then True
     else isWeakAncestorDir suspected_ancestor $ takeDirectory dir_of_interest
+
+appendSlashToDir :: FilePath -> FilePath -> IO FilePath
+appendSlashToDir dirname filename = do
+  isdir <- doesDirectoryExist $ dirname </> filename
+  if isdir
+    then return $ addTrailingPathSeparator filename
+    else return filename
