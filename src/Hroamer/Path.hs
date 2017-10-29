9dbc492ab3f5655b45fa61fe93a7ac3965359a3b
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2017-10-29 20:58:02 +0800

    [R] Shift createDirNoForce to Hroamer.Path module
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- src/Hroamer/Path.hs
+++ src/Hroamer/Path.hs
@@ -1,10 +1,12 @@
 module Hroamer.Path
   ( appendSlashToDir
+  , createDirNoForce
   , isWeakAncestorDir
   ) where
 
 import Foundation
-import System.Directory (doesDirectoryExist)
+import System.Directory
+       (createDirectory, doesDirectoryExist, doesPathExist)
 import System.FilePath.Posix
        (FilePath, (</>), addTrailingPathSeparator, takeDirectory)
 
@@ -21,3 +23,20 @@ appendSlashToDir dirname filename = do
   if isdir
     then return $ addTrailingPathSeparator filename
     else return filename
+
+createDirNoForce :: FilePath -> IO Bool
+createDirNoForce app_data_dir = do
+  path_exists <- doesPathExist app_data_dir
+  if path_exists
+    then do
+      is_dir <- doesDirectoryExist app_data_dir
+      if is_dir
+        then return True
+        else do
+          putStrLn $
+            "Error: `" <> (show app_data_dir) <>
+            "` exists but is not a directory."
+          return False
+    else do
+      createDirectory app_data_dir
+      return True
