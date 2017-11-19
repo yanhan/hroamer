d2341eb3d26d1d78f49e5fd0d2eae510a1215bb2
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2017-11-19 11:20:14 +0800

    [TEST] Hroamer.Parser
    Not the best of tests, but better than nothing
    
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

---
+++ test/Hroamer/ParserSpec.hs
@@ -0,0 +1,30 @@
+module Hroamer.ParserSpec
+  ( spec
+  ) where
+
+import Data.Either (either)
+import Foundation
+import Test.Hspec (Spec, describe, it, shouldBe)
+import Text.Parsec (ParseError(..), runParser)
+
+import Hroamer.Parser (parseDirStateLine)
+
+spec :: Spec
+spec = do
+  describe "parseDirStateLine" $ do
+    it "should return (Right Nothing) for a comment line" $
+      runParser parseDirStateLine () ""  "\" just a comment"  `shouldBe` Right Nothing
+
+    it "should return (Right filename, uuid)) for a line which contains a filename and uuid separated by \" | \"" $ do
+      let filename = "main.c"
+      let uuid = "51z9125f-351c-4610-125m-n15jfa086j4h"
+      runParser parseDirStateLine () ""  (filename <> " | " <> uuid) `shouldBe`
+        (Right $ Just (filename, uuid))
+
+    it "should return Left ParseError for a line which does not fulfil the specifications" $ do
+      let filename = "special.a"
+      let invalidUuid = "c0aa744f664a7157465c9ac52b87d044"
+      either
+        (const $ True `shouldBe` True)
+        (const $ True `shouldBe` False)
+        (runParser parseDirStateLine () ""  (filename <> " | " <> invalidUuid))
