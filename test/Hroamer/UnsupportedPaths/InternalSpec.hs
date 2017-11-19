dd9682c31c26b9df55befc58c60d04dcc4cce452
Pang Yan Han <pangyanhan@gmail.com>
2017-11-19 16:48:08 +0800

    [TEST] Hroamer.UnsupportedPaths.Internal.formatPathsForErrorMessage
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

---
+++ test/Hroamer/UnsupportedPaths/InternalSpec.hs
@@ -0,0 +1,17 @@
+module Hroamer.UnsupportedPaths.InternalSpec
+  ( spec
+  ) where
+
+import Foundation
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+import Hroamer.UnsupportedPaths.Internal
+       (formatPathsForErrorMessage)
+
+spec :: Spec
+spec = do
+  describe "formatPathsForErrorMessage" $ do
+    it "should sort the FilePaths, convert them to Text and prepend them with a '- '" $
+      let paths = ["number-theory-hw01.pdf", "a.txt", "main.c", "charges.txt"]
+      in formatPathsForErrorMessage paths `shouldBe`
+         ["- a.txt", "- charges.txt", "- main.c", "- number-theory-hw01.pdf"]
