bd75b045b91ad9ba756f85284a244d91008b85eb
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2017-11-19 15:53:43 +0800

    [R] UnsupportedPaths: shift error messages to Internal module
    To facilitate testing
    
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- lib/Hroamer/UnsupportedPaths/Internal.hs
+++ lib/Hroamer/UnsupportedPaths/Internal.hs
@@ -1,8 +1,13 @@
 module Hroamer.UnsupportedPaths.Internal
   ( UPaths(..)
+  , absolutePathsErrorTitle
+  , duplicatePathsErrorTitle
+  , invalidPathsErrorTitle
+  , relativePathsErrorTitle
   ) where
 
 import Data.Set (Set)
+import Data.Text (Text)
 import Foundation
 import System.FilePath.Posix (FilePath)
 
@@ -13,3 +18,18 @@ data UPaths = UPaths
   , filesNotInCwd :: Set FilePath
   , invalidPaths :: Set FilePath
   } deriving (Eq, Show)
+
+duplicatePathsErrorTitle :: Text
+duplicatePathsErrorTitle = "Error: the following filenames are duplicated:"
+
+absolutePathsErrorTitle :: Text
+absolutePathsErrorTitle =
+  "Error: Absolute paths not supported. We found that you entered these:"
+
+relativePathsErrorTitle :: Text -> Text
+relativePathsErrorTitle cwd =
+  "Error: the following paths are housed in a directory that is not the current directory " <>
+  cwd
+
+invalidPathsErrorTitle :: Text
+invalidPathsErrorTitle = "Error: the following paths are invalid:"
