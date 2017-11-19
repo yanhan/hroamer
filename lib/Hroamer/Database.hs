fc6c088ee63725ce841000b01c1e9d95ba455b75
Pang Yan Han <pangyanhan@gmail.com>
2017-11-19 18:22:39 +0800

    [R] Create Hroamer.Database.Internal module
    So that we can test the `addFileDetailsToDb` function
    
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- lib/Hroamer/Database.hs
+++ lib/Hroamer/Database.hs
@@ -12,24 +12,13 @@ module Hroamer.Database
 import Data.Text (Text)
 import Database.SQLite.Simple
        (Connection, execute, execute_, query, withConnection)
-import Database.SQLite.Simple.FromRow (FromRow, field, fromRow)
-import Database.SQLite.Simple.ToRow (ToRow, toRow)
 import Foundation
 import Foundation.Collection (mapM_)
 import System.Directory (doesFileExist)
 import System.FilePath.Posix (FilePath)
 
-data FilesTableRow = FilesTableRow
-  { dir :: FilePath
-  , filename :: FilePath
-  , uuid :: Text
-  } deriving (Eq, Show)
-
-instance FromRow FilesTableRow where
-  fromRow = FilesTableRow <$> field <*> field <*> field
-
-instance ToRow FilesTableRow where
-  toRow ftr = toRow (dir ftr, filename ftr, uuid ftr)
+import Hroamer.Database.Internal
+       (FilesTableRow(FilesTableRow, dir, filename, uuid), addFileDetailsToDb)
 
 createDbAndTables :: FilePath -> IO ()
 createDbAndTables path_to_db = do
@@ -44,12 +33,6 @@ createDbAndTables path_to_db = do
               execute_ conn "CREATE INDEX files__idx_dir ON files(dir);")
     else return ()
 
-addFileDetailsToDb :: FilePath -> Connection -> ([Char], Text) -> IO ()
-addFileDetailsToDb dir conn (filename, uuid) =
-  execute
-    conn
-    "INSERT INTO files(dir, filename, uuid) VALUES(?, ?, ?);"
-    (FilesTableRow {dir = dir, filename = filename, uuid = uuid})
 
 deleteFileFromDb :: FilePath -> Connection -> [Char] -> IO ()
 deleteFileFromDb cwd conn filename =
