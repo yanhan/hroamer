bc42f2bf133a03d48ac5f7233f434cb5c9a1ec0d
Pang Yan Han <pangyanhan@gmail.com>
2017-11-24 22:38:03 +0800

    [R] DatabaseSpec: create temp dir and db once (mostly)
    To hopefully save up on some IO operations and let our tests run a
    tiny bit faster. Also learnt how to use the beforeAll, afterAll and
    after hooks provided by hspec.
    
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- test/Hroamer/DatabaseSpec.hs
+++ test/Hroamer/DatabaseSpec.hs
@@ -4,13 +4,19 @@ module Hroamer.DatabaseSpec
 
 import Control.Monad (mapM_)
 import qualified Data.List as List
-import Database.SQLite.Simple (withConnection)
+import Database.SQLite.Simple (execute_, withConnection)
+import Data.Text (pack)
+import qualified Data.Text.IO as TIO
 import Foundation
-import System.Directory (doesFileExist, doesPathExist)
-import System.FilePath.Posix ((</>))
+import System.Directory
+       (doesFileExist, doesPathExist, removeDirectory, removeFile)
+import System.FilePath.Posix ((</>), FilePath, takeDirectory)
 import System.IO (readFile)
-import System.IO.Temp (withSystemTempDirectory, writeTempFile)
-import Test.Hspec (Spec, describe, it, shouldBe, shouldReturn)
+import System.IO.Temp
+       (createTempDirectory, withSystemTempDirectory, writeTempFile)
+import Test.Hspec
+       (Spec, after, afterAll, beforeAll, describe, it, shouldBe,
+        shouldReturn)
 
 import Hroamer.Database
        (createDbAndTables, getAllFilesInDir, getRowFromUUID,
@@ -19,6 +25,22 @@ import Hroamer.Database.Internal
        (FilesTableRow(FilesTableRow, dir, filename, uuid),
         addFileDetailsToDb)
 import TestHelpers (getTotalRows)
+
+setupDbForTest :: IO FilePath
+setupDbForTest = do
+  dirPath <- createTempDirectory "/tmp"  "DatabaseSpec"
+  let pathToDb = dirPath </> "hroamer.db"
+  createDbAndTables pathToDb
+  return pathToDb
+
+deleteTempDirForTest :: FilePath -> IO ()
+deleteTempDirForTest pathToDb = do
+  removeFile pathToDb
+  removeDirectory $ takeDirectory pathToDb
+
+clearDb :: FilePath -> IO ()
+clearDb pathToDb = withConnection pathToDb (\dbconn ->
+  execute_ dbconn "DELETE FROM files;")
 
 spec :: Spec
 spec = do
@@ -39,14 +61,12 @@ spec = do
         doesFileExist pathToDb `shouldReturn` True
         readFile pathToDb `shouldReturn` contents
 
-  describe "getAllFilesInDir" $ do
-    it "should return all rows whose `dir` equal to the given value" $ do
-      withSystemTempDirectory "getAllFilesInDir" $ \dirPath -> do
-        let pathToDb = dirPath </> "hroamer.db"
+  beforeAll setupDbForTest $ afterAll deleteTempDirForTest $ after clearDb $ do
+    describe "getAllFilesInDir" $ do
+      it "should return all rows whose `dir` equal to the given value" $ \pathToDb -> do
         let dirOfInterest = "/home/betty/irc/real"
         let firstTuple = ("creds.txt", "eae274ee-9411-4dcf-8465-dd5dd5155087")
         let secondTuple = ("arena.txt", "c304d170-57a0-4d72-bcae-e59a4177b69d")
-        createDbAndTables pathToDb
         withConnection pathToDb (\dbconn -> do
           addFileDetailsToDb dbconn dirOfInterest firstTuple
           addFileDetailsToDb dbconn "/opt/local/bin" ("storm", "bd45efb5-2bb7-4dcf-86ce-a262ed958e7a")
@@ -55,14 +75,11 @@ spec = do
         length fileDetails `shouldBe` 2
         List.sort fileDetails `shouldBe` [secondTuple, firstTuple]
 
-  describe "getRowFromUUID" $ do
-    it "should retrieve the row with the uuid" $ do
-      withSystemTempDirectory "getRowFromUUID" $ \dirPath -> do
-        let pathToDb = dirPath </> "hroamer.db"
+    describe "getRowFromUUID" $ do
+      it "should retrieve the row with the uuid" $ \pathToDb -> do
         let dirOfInterest = "/home/fire/is/at"
         let fileOfInterest = "somewhere"
         let uuidOfInterest = "9cef0b1c-113b-4b51-88f2-1f93d9178a15"
-        createDbAndTables pathToDb
         withConnection pathToDb (\dbconn -> do
           addFileDetailsToDb dbconn "/usr/share/man/man1" ("make.1.gz", "0ae251b2-bd1d-41c6-b574-40198710e9e3")
           addFileDetailsToDb dbconn dirOfInterest (fileOfInterest, uuidOfInterest)
@@ -71,16 +88,13 @@ spec = do
           listToMaybe row `shouldBe`
             Just (FilesTableRow dirOfInterest fileOfInterest uuidOfInterest))
 
-  describe "updateDirAndFilename" $ do
-    it "should update the dir and filename of the row that has the given uuid" $ do
-      withSystemTempDirectory "updateDirAndFilename" $ \dirPath -> do
-        let pathToDb = dirPath </> "hroamer.db"
+    describe "updateDirAndFilename" $ do
+      it "should update the dir and filename of the row that has the given uuid" $ \pathToDb -> do
         let originalDir = "/my/imaginary/dir/"
         let originalFile = "ledger.c"
         let newDir = "/favorite/pasttimes"
         let newFile = "movies"
         let uuid = "5730152c-9a3f-4b4e-b373-62ea1a2abb4f"
-        createDbAndTables pathToDb
         withConnection pathToDb (\dbconn -> do
           addFileDetailsToDb dbconn originalDir (originalFile, uuid)
           addFileDetailsToDb dbconn "/main/man" ("yu", "45889947-8104-4404-a704-7d59d2bd1aed")
@@ -90,16 +104,13 @@ spec = do
           listToMaybe row `shouldBe`
             Just (FilesTableRow newDir newFile uuid))
 
-  describe "updateDbToMatchDirState" $ do
-    it "should insert into the table files only on the system and delete from the table files not on the system" $ do
-      withSystemTempDirectory "updateDbToMatchDirState" $ \dirPath -> do
-        let pathToDb = dirPath </> "hroamer.db"
+    describe "updateDbToMatchDirState" $ do
+      it "should insert into the table files only on the system and delete from the table files not on the system" $ \pathToDb -> do
         let dirOfInterest = "/going/to"
         let ftrOne = FilesTableRow "/leaving/this"  "alone"  "855fba5b-8c27-41a0-9d0b-aa60e25f4193"
         let ftrTwo = FilesTableRow dirOfInterest  "be-created"  "91bd704f-fb43-4cd7-8eb6-3afc08086d72"
         let ftrThree = FilesTableRow dirOfInterest "ecstatic"  "dd132727-df0f-405f-bcd1-f972916bcb4e"
         let ftrFour = FilesTableRow dirOfInterest  "destination"  "1bcaf24f-8a88-4377-ae15-8beaf75a2b4f"
-        createDbAndTables pathToDb
         withConnection pathToDb (\dbconn -> do
           addFileDetailsToDb dbconn dirOfInterest ("delete", "ce053cea-bd64-42c6-b3ed-13931a917017")
           addFileDetailsToDb dbconn (dir ftrOne) (filename ftrOne, uuid ftrOne)
@@ -119,10 +130,8 @@ spec = do
               getRowFromUUID dbconn (uuid ftr) >>= (shouldBe $ Just ftr) . listToMaybe)
              [ftrOne, ftrTwo, ftrThree, ftrFour])
 
-  describe "wrapDbConn" $ do
-    it "will encapsulate a db connection so workhorse functions don't have to handle it" $ do
-      withSystemTempDirectory "wrapDbConn" $ \dirPath -> do
-        let pathToDb = dirPath </> "hroamer.db"
+    describe "wrapDbConn" $ do
+      it "will encapsulate a db connection so workhorse functions don't have to handle it" $ \pathToDb -> do
         let uuid = "e3e8bbc5-f2d5-4674-a958-c88aabf392cc"
         let dirname = "/let/there/be"
         let fname = "dragons"
