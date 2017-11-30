4b1996cbe43b9168bc0f6f51aeff24aaa6e315b8
Yan Han Pang <539739+yanhan@users.noreply.github.com>
2017-12-01 07:56:42 +0800

    [R] StateFileSpec: create one app dir for each spec
    Signed-off-by: Yan Han Pang <539739+yanhan@users.noreply.github.com>

--- test/Hroamer/StateFileSpec.hs
+++ test/Hroamer/StateFileSpec.hs
@@ -3,7 +3,10 @@ module Hroamer.StateFileSpec
   ) where
 
 import Data.List (unlines)
-import Foundation
+import Data.Map.Strict (Map, empty, fromList, lookup)
+import Data.Maybe (fromJust)
+import Data.Text (Text)
+import Foundation hiding (fromList)
 import System.Directory (createDirectory)
 import System.FilePath ((</>), FilePath)
 import System.IO (readFile, writeFile)
@@ -13,23 +16,27 @@ import Test.Hspec (Spec, afterAll, beforeAll, describe, it, parallel, shouldRetu
 
 import qualified Hroamer.StateFile as StateFile
 
-createDirsForTest :: IO (FilePath, FilePath)
+createDirsForTest :: IO (FilePath, Map Text FilePath)
 createDirsForTest = do
   cwd <- createTempDirectory "/tmp"  "StateFileSpec"
-  appTmpDir <- createTempDirectory "/tmp"  "StateFileSpecApp"
-  return (cwd, appTmpDir)
+  appTmpDirOne <- createTempDirectory "/tmp"  "StateFile.create"
+  appTmpDirTwo <- createTempDirectory "/tmp"  "StateFile.read"
+  let m = fromList [ ("StateFile.create", appTmpDirOne)
+                   , ("StateFile.read", appTmpDirTwo)
+                   ]
+  return (cwd, m)
 
-rmrf :: (FilePath, FilePath) -> IO ()
-rmrf (fileOne, fileTwo) = do
-  let rmrfProc = proc "/bin/rm" ["-rf", toList fileOne, toList fileTwo]
+rmrf :: (FilePath, a) -> IO ()
+rmrf (dir, _) = do
+  let rmrfProc = proc "/bin/rm" ["-rf", toList dir]
   (_, _, _, ph) <- createProcess rmrfProc
   waitForProcess ph
   return ()
 
 spec :: Spec
-spec = beforeAll createDirsForTest $ afterAll rmrf $ do
+spec = parallel $ beforeAll createDirsForTest $ afterAll rmrf $ do
   describe "StateFile.create" $ do
-    it "should create a correct state file" $ \(cwd, appTmpDir) -> do
+    it "should create a correct state file" $ \(cwd, appTmpDirMap) -> do
       let dirToCreatePair = ("dinosaur", "10918d1c-284b-414a-aa1b-400074abace4")
       let fileToCreatePair = ("minions", "9bc243bf-a1b4-4029-af75-f4391fe34471")
       let dotXhrcPair = (".xhrc", "2e67d5b0-6f33-4207-98db-ab0a3a4a5190")
@@ -43,6 +50,7 @@ spec = beforeAll createDirsForTest $ afterAll rmrf $ do
                               , fileToCreatePair
                               , dotXhrcPair
                               ]
+      let appTmpDir = fromJust $ lookup "StateFile.create" appTmpDirMap
       stateFilePath <- StateFile.create cwd appTmpDir filesAndUuidInDir
       let contents = unlines $ [
                        "\" pwd: " <> (toList cwd)
@@ -53,7 +61,7 @@ spec = beforeAll createDirsForTest $ afterAll rmrf $ do
       fmap (<> "\n") (readFile stateFilePath) `shouldReturn` contents
 
   describe "StateFile.read" $ do
-    it "should extract all that was written by the create function" $ \(cwd, appTmpDir) -> do
+    it "should extract all that was written by the create function" $ \(cwd, appTmpDirMap) -> do
       let pairA = ("good-Night", "2436798f-2b60-4d97-87f5-f1e91e69e455")
       let pairB = (".main-thing", "445685f1-5faa-4bbe-8a04-c26dd4098738")
       let pairC = ("FoolsErrand", "2a04c542-3e06-474f-a7d6-4cee6ceaa583")
@@ -61,5 +69,6 @@ spec = beforeAll createDirsForTest $ afterAll rmrf $ do
       -- Create pairB as a dir
       createDirectory $ cwd </> (fst pairB)
       let filesAndUuidInDir = [pairA, pairB, pairC, pairD]
+      let appTmpDir = fromJust $ lookup "StateFile.read" appTmpDirMap
       stateFilePath <- StateFile.create cwd appTmpDir filesAndUuidInDir
       StateFile.read stateFilePath `shouldReturn` [pairB, pairC, pairA, pairD]
